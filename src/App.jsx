7279556dd0a666c161948982bf320873c8d3c35d
Mighty <realofficialvictor@gmail.com>
2025-10-14 12:11:29 +0100

    content completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
 import { HeaderLogoNav } from './component/Header'
+import { Content } from './component/content'
+import './App.css'
+
 
 
 function App() {
   return (
     <div className='app-container'>
       <HeaderLogoNav />
+      <Content />
     </div>
   )
 }
--- src/component/content.jsx
+++ src/component/content.jsx
@@ -1,10 +1,23 @@
+import NextIcon from '../assets/next icon.png'
+import MegaPhoneIcon from '../assets/megaphone icon.png'
 
 export function Content() {
   return (
     <div className="content-container" >  
       <div className="next-container" >
-        <img src="" alt="" />
-
+        <img src={MegaPhoneIcon} alt="" />
+        <p className="next-text" >Now supporting Next.Js 15.4</p>
+        <img src={NextIcon} alt="" />
+      </div>
+      <div className='header-text' >
+        <h1>Grow Organically Without Doing Anything</h1>
+      </div>
+      <div className='paragraph-text' >
+        <p>Flux grow is an autonomous content engine that delivers full blog optimization without any manual input, freeing businesses to focus on what matters.</p>
+      </div>
+      <div className='content-buttons' >
+        <button>Start your Project</button>
+          <button>Request a Demo<img src={NextIcon} alt="" /></button>
       </div>
     </div>
   )
