e8e5506795bbfa7269907fa893bd10bef70753b4
Mighty <realofficialvictor@gmail.com>
2025-10-14 12:44:14 +0100

    truested by jsx completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { HeaderLogoNav } from './component/Header'
 import { Content } from './component/content'
+import { TrustedBy } from './component/Trustedby'
 import './App.css'
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className='app-container'>
       <HeaderLogoNav />
       <Content />
+      <TrustedBy />
     </div>
   )
 }
--- src/component/Trustedby.jsx
+++ src/component/Trustedby.jsx
@@ -1,9 +1,18 @@
-
+import ComapnyLogo1 from '../assets/1.png'
+import ComapnyLogo2 from '../assets/2.png'
+import ComapnyLogo3 from '../assets/3.png'
+import ComapnyLogo4 from '../assets/4.png'
+import ComapnyLogo5 from '../assets/5.png'
 
 export function TrustedBy() {
   return (
     <div className="trusted-by" >
-      
+      <img src={ComapnyLogo1} alt="" />
+      <img src={ComapnyLogo2} alt="" />
+      <img src={ComapnyLogo3} alt="" />
+      <img src={ComapnyLogo4} alt="" />
+      <img src={ComapnyLogo5} alt="" />
+      <p>50+ Companies</p>
     </div>
   )
 }
