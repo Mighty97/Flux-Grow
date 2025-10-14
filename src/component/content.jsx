676414448d84a430aa08cc170f8c07c889233d67
Mighty <realofficialvictor@gmail.com>
2025-10-14 11:37:45 +0100

    content component created

---
+++ src/component/content.jsx
@@ -0,0 +1,11 @@
+
+export function Content() {
+  return (
+    <div className="content-container" >  
+      <div className="next-container" >
+        <img src="" alt="" />
+
+      </div>
+    </div>
+  )
+}
